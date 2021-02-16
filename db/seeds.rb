829d19b418780d86e02c186908969a19fdebbb33
frederic <fredericseguier@yahoo.fr>
2021-02-16 14:55:36 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,28 @@
+require 'faker'
 User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
+
 5.times do |index|
-  User.create!(first_name: Faker::Games::Overwatch.hero,
-               last_name: Faker::Games::Overwatch.location,
+  User.create!(first_name: Faker::Games::SuperMario.character,
+               last_name: Faker::Games::SuperMario.character,
                email: "users#{index}" + "@yopmail.com",
                password: "coucoucoucou",
                description: Faker::Lorem.paragraph_by_chars(number: rand(20..50)))
 end
 
-Event.destroy_all
+
 10.times do
   Event.create!(start_date: Faker::Date.between(from: "2021-02-20", to: "2021-09-25"),
                 duration: [30, 60, 90, 120, 180, 210].sample,
-                title: Faker::Lorem.paragraph_by_chars(number: rand(5..140)),
-                description: Faker::Lorem.paragraph_by_chars(number: rand(20..1000)),
+                title: Faker::Movies::HarryPotter.spell ,
+                description: Faker::Movies::HarryPotter.quote,
                 price: rand(1..1000),
                 location: Faker::Address.city,
                 admin: User.all.sample)
 end
 
-Attendance.destroy_all
+
 10.times do
   Attendance.create!(user: User.all.sample,
                      event: Event.all.sample)
